3a73da4957a70283da8041bb17c7001cd8935f14
Marek Hanzal <marek.hanzal@x32.cz>
2022-10-13 21:35:02 +0200

    Fixed toHumanBytes method

--- src/utils/toHumanBytes.ts
+++ src/utils/toHumanBytes.ts
@@ -4,4 +4,4 @@ import {filesize} from "filesize";
  * @TODO Move this to FileService
  * @param size
  */
-export const toHumanBytes = (size: number) => filesize(size);
+export const toHumanBytes = (size: number) => filesize(size) as string;
