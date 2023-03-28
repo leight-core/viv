6fd550431a28243a6cefa1864185a4a8ea35bce1
Marek Hanzal <marek.hanzal@x32.cz>
2023-03-28 19:59:16 +0200

    Improved docs?

---
+++ apps/nextra/CHANGELOG.md
@@ -0,0 +1,31 @@
+# @leight/docs
+
+## 0.5.5
+
+### Patch Changes
+
+- Update
+
+## 0.5.4
+
+### Patch Changes
+
+- Fixed base path
+
+## 0.5.3
+
+### Patch Changes
+
+- Fixed next.js app
+
+## 0.5.2
+
+### Patch Changes
+
+- Trying to fix next.js startup
+
+## 0.5.1
+
+### Patch Changes
+
+- Initial attempt to bundle next.js app
---
+++ apps/nextra/next.config.mjs
@@ -0,0 +1,18 @@
+import nextra from "nextra";
+
+export const config = nextra({
+    theme:       "nextra-theme-docs",
+    themeConfig: "./theme.config.tsx",
+})({
+    basePath: process.env.BASE_URL,
+    images:   {
+        unoptimized:    true,
+        remotePatterns: [
+            {
+                hostname: "avatars.githubusercontent.com",
+            },
+        ],
+    },
+});
+
+export default config;
