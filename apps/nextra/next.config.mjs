fdfc5e8b1411876da154cc83f8448e59e137a335
Marek Hanzal <marek.hanzal@x32.cz>
2023-03-31 17:02:05 +0200

    ...

--- apps/nextra/next.config.mjs
+++ apps/nextra/next.config.mjs
@@ -4,8 +4,11 @@ export const config = nextra({
     theme:       "nextra-theme-docs",
     themeConfig: "./theme.config.tsx",
 })({
-    basePath: process.env.BASE_URL,
-    images:   {
+    basePath:     process.env.BASE_URL,
+    experimental: {
+        turbo: {},
+    },
+    images:       {
         unoptimized:    true,
         remotePatterns: [
             {
